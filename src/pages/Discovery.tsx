bc716baed006ea8869fc0644a01f7105e842e4b3
craigrbarnes <craigrbarnes@uchicago.edu>
2024-02-13 00:18:12 -0600

    fix @lib build error

--- src/pages/Discovery.tsx
+++ src/pages/Discovery.tsx
@@ -2,8 +2,8 @@ import {
   DiscoveryPage,
   DiscoveryPageGetServerSideProps as getServerSideProps,
 } from '@gen3/frontend';
-import { registerDiscoveryCustomCellRenderers } from '@/lib/Discovery/CustomCellRenderers';
-import { registerDiscoveryStudyPreviewRenderers } from '@/lib/Discovery/CustomRowRenderers';
+import { registerDiscoveryCustomCellRenderers } from '../lib/Discovery/CustomCellRenderers';
+import { registerDiscoveryStudyPreviewRenderers } from '../lib/Discovery/CustomRowRenderers';
 
 registerDiscoveryCustomCellRenderers();
 registerDiscoveryStudyPreviewRenderers();
