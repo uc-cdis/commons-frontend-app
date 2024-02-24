a78614b7ded627c8033af24cb2e45032108dc831
craigrbarnes <craigrbarnes@uchicago.edu>
2024-02-23 23:01:34 -0600

    update dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 'use strict';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const dns = require('dns');
+
+dns.setDefaultResultOrder('ipv4first');
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('./src/lib/plugins/index.js');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -18,14 +23,26 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-  transpilePackages: ['@gen3/frontend'],
   basePath: process.env.BASE_PATH || '',
+  transpilePackages: ['@gen3/core', '@gen3/frontend'],
   webpack: (config) => {
     config.infrastructureLogging = {
       level: "error",
     };
-
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)?", // Matches all pages
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
   },
 };
 
