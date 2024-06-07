93864fabaed70c11883670d9b8d20d47b190436b
craigrbarnes <craig.barnes@intelligentlight.com>
2024-06-07 11:23:15 -0500

    update config/package.json

--- src/lib/Discovery/CustomRowRenderers.tsx
+++ src/lib/Discovery/CustomRowRenderers.tsx
@@ -10,74 +10,75 @@ import {
   getTagColor,
 } from '@gen3/frontend';
 
-// TODO: This is a hack to get around the fact that the data is not typed
+
 interface TagData {
   name: string;
   category: string;
 }
 
 const DetailsWithTagsRowRenderer =
-    (
-        { row } : RowRenderFunctionParams,
-        studyPreviewConfig?: StudyDetailsField,
-    ): ReactElement => {
-      const { discoveryConfig: config, setStudyDetails } = useDiscoveryContext();
+  (
+    { row } : RowRenderFunctionParams,
+    studyPreviewConfig?: StudyDetailsField,
+  ): ReactElement => {
+    const { discoveryConfig: config, setStudyDetails } = useDiscoveryContext();
 
-      if (!studyPreviewConfig) {
-        return <React.Fragment></React.Fragment>;
-      }
-      const value =
-          JSONPath({
-            json: row.original,
-            path: studyPreviewConfig.field,
-          }) ??
-          config?.studyPreviewField?.valueIfNotAvailable ??
-          '';
+    if (!studyPreviewConfig) {
+      return <React.Fragment></React.Fragment>;
+    }
+    const value =
+      JSONPath({
+        json: row.original,
+        path: studyPreviewConfig.field,
+      }) ??
+      config?.studyPreviewField?.valueIfNotAvailable ??
+      '';
 
-      return (
-          <Box
-              sx={{
-                display: 'flex',
-                width: '100%',
-              }}
-              onClick={() => {
-                setStudyDetails(() => {
-                  return { ...row.original };
-                });
-              }}
-          >
-            <div className="flex flex-col">
-              <Text size="sm" lineClamp={2}>
-                {value}
-              </Text>
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+        }}
+        onClick={() => {
+          setStudyDetails(() => {
+            return { ...row.original };
+          });
+        }}
+      >
+        <div className="flex flex-col">
+          <Text size="sm" lineClamp={2}>
+            {value}
+          </Text>
 
-              <div className="flex space-x-6 space-y-6 flex-wrap">
-                {row.original?.tags.map(({ name, category }: TagData) => {
-                  const color = getTagColor(category, config.tagCategories);
-                  return (
-                      <Badge
-                          role="button"
-                          size="lg"
-                          radius="sm"
-                          variant="outline"
-                          tabIndex={0}
-                          aria-label={name}
-                          key={name}
-                          style={{
-                            borderColor: color,
-                            borderWidth: '3px',
-                            margin: '0 0.125rem',
-                          }}
-                      >
-                        {name}
-                      </Badge>
-                  );
-                })}
-              </div>
-            </div>
-          </Box>
-      );
-    };
+          <div className="flex space-x-6 space-y-6 flex-wrap">
+            {row.original?.tags.map(({ name, category }: TagData) => {
+              const color = getTagColor(category, config.tagCategories);
+              if (name === '') return null; // no tag
+              return (
+                  <Badge
+                    role="button"
+                    size="lg"
+                    radius="sm"
+                    variant="outline"
+                    tabIndex={0}
+                    aria-label={name}
+                    key={name}
+                    style={{
+                      borderColor: color,
+                      borderWidth: '3px',
+                      margin: '0 0.125rem',
+                    }}
+                  >
+                    {name}
+                  </Badge>
+              );
+            })}
+          </div>
+        </div>
+      </Box>
+    );
+  };
 
 export default DetailsWithTagsRowRenderer;
 
