2759768a25095edda22099d90e01f91dad93d3ea
craigrbarnes <craig.barnes@intelligentlight.com>
2024-06-07 12:40:24 -0500

    add eslint/prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,61 +24,61 @@ interface Gen3AppProps {
 }
 
 const Gen3App = ({
-                   Component,
-                   pageProps,
-                   colors,
-                   icons,
-                   themeFonts,
-                   sessionConfig,
-                   modalsConfig,
-                 }: AppProps & Gen3AppProps) => {
+  Component,
+  pageProps,
+  colors,
+  icons,
+  themeFonts,
+  sessionConfig,
+  modalsConfig,
+}: AppProps & Gen3AppProps) => {
   useEffect(() => {
     setDRSHostnames(drsHostnames);
   }, []);
 
   return (
-      <Gen3Provider
-          colors={colors}
-          icons={icons}
-          fonts={themeFonts}
-          sessionConfig={sessionConfig}
-          modalsConfig={modalsConfig}
-      >
-        <Component {...pageProps} />
-      </Gen3Provider>
+    <Gen3Provider
+      colors={colors}
+      icons={icons}
+      fonts={themeFonts}
+      sessionConfig={sessionConfig}
+      modalsConfig={modalsConfig}
+    >
+      <Component {...pageProps} />
+    </Gen3Provider>
   );
 };
 
 // TODO: replace with page router
 Gen3App.getInitialProps = async (
-    context: AppContext,
+  context: AppContext,
 ): Promise<Gen3AppProps & AppInitialProps> => {
   const ctx = await App.getInitialProps(context);
 
   try {
     const modals = await ContentSource.get(
-        `config/${GEN3_COMMONS_NAME}/modals.json`,
+      `config/${GEN3_COMMONS_NAME}/modals.json`,
     );
     const session = await ContentSource.get(
-        `config/${GEN3_COMMONS_NAME}/session.json`,
+      `config/${GEN3_COMMONS_NAME}/session.json`,
     );
 
     const fonts = await ContentSource.get(
-        `config/${GEN3_COMMONS_NAME}/themeFonts.json`,
+      `config/${GEN3_COMMONS_NAME}/themeFonts.json`,
     );
 
     const themeColors = await ContentSource.get(
-        `config/${GEN3_COMMONS_NAME}/themeColors.json`,
+      `config/${GEN3_COMMONS_NAME}/themeColors.json`,
     );
 
     const colors = Object.fromEntries(
-        Object.entries(themeColors).map(([key, values]) => [
-          key,
-          Object.values(values) as TenStringArray,
-        ]),
+      Object.entries(themeColors).map(([key, values]) => [
+        key,
+        Object.values(values) as TenStringArray,
+      ]),
     );
 
-    const icons = await ContentSource.get(`config/icons/gen3.json`);
+    const icons = await ContentSource.get('config/icons/gen3.json');
     return {
       ...ctx,
       modalsConfig: modals,
@@ -87,8 +87,10 @@ Gen3App.getInitialProps = async (
       colors: colors,
       icons: icons as RegisteredIcons,
     };
-  } catch (error: any) {
-    console.error('Provider Wrapper error loading config', error.toString());
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Provider Wrapper error loading config', error.toString());
+    } else console.error('Provider wrapper: unknown error');
   }
   // return default
   return {
