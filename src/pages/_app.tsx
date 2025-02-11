f006b2d4d0de5d53b48b09ad13aa482bc729b351
Craig Barnes <craig.barnes@intelligentlight.com>
2025-02-11 16:24:21 -0600

    update to 0.10.80 (#73)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,8 +13,7 @@ import {
   Fonts,
   createMantineTheme,
   SessionConfiguration,
-  // registerCohortDiscoveryApp,
-  registerCohortDiversityApp,
+  registerExplorerDefaultCellRenderers,
   registerCohortBuilderDefaultPreviewRenderers,
   registerMetadataSchemaApp,
 } from '@gen3/frontend';
@@ -71,9 +70,8 @@ const Gen3App = ({
     //   !faroRef.current
     // ) {
     if (!faroRef.current) faroRef.current = initGrafanaFaro();
-    //  registerCohortDiscoveryApp();
-    registerCohortDiversityApp();
     registerMetadataSchemaApp();
+    registerExplorerDefaultCellRenderers();
     registerCohortBuilderDefaultPreviewRenderers();
     registerCohortTableCustomCellRenderers();
     registerCustomExplorerDetailsPanels();
