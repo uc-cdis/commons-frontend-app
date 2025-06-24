cf520a74e32b87cacbd5c50294ec72578ac7b840
Shawn O'Connor <ocshawn@ocshawn.com>
2025-06-24 10:09:58 -0500

    (CADC-1801): add a logo to the browser tab too for QA (#64)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
   return (
     <Html lang="en" {...mantineHtmlProps}>
       <Head>
+        <link rel="icon" type="image/png" href="/icons/favicon.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/icons/cadc_32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/icons/cadc_48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/png" href="/icons/cadc_64x64.png" sizes="64x64" />
         <ColorSchemeScript defaultColorScheme="auto" />
       </Head>
       <body>
