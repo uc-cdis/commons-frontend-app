b2f893dc31ea191d56ed1827550012bc958dbd07
craigrbarnes <craig.barnes@intelligentlight.com>
2025-11-06 00:54:13 -0600

    add landing page

---
+++ src/components/ArrowLink.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, Image } from '@mantine/core';
+
+interface ArrowLinkProps {
+  href: string;
+  text: string;
+}
+
+const ArrowLink = ({ href, text }: ArrowLinkProps) => {
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="w-[24px] h-[24px] relative overflow-hidden">
+        <Image
+          src="/icons/arrow-right.svg"
+          alt="Right pointer red arrow"
+          w="auto"
+          fit="contain"
+          height={24}
+        />
+      </div>
+    <a href={href}>
+      <Text c="black" td="underline" fw={700} className="text-2xl">{text}</Text>
+
+    </a>
+    </div>
+  );
+};
+
+export default ArrowLink;
