6d4326393c1b143f362fefaa3e1db579d74d15c1
craigrbarnes <craig.barnes@intelligentlight.com>
2026-09-22 10:00:46 -0500

    Update .env and remove unused lw-workspace proxy route in next.config.js.

--- next.config.js
+++ next.config.js
@@ -94,10 +94,6 @@ const nextConfig = {
           destination: `${GEN3_TARGET}/authz/:path*`,
         },
         {
-          source: '/lw-workspace/proxy/',
-          destination: `${GEN3_TARGET}/lw-workspace/proxy/`,
-        },
-        {
           source: '/lw-workspace/:path*',
           destination: `${GEN3_TARGET}/lw-workspace/:path*`,
         },
